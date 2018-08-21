7a7123e14506bd5b6f5c4267065d1add3564f0d6
Gautam J <34481342+Gautam-J@users.noreply.github.com>
2018-08-21 17:08:10 +0530

    Update 4) Dog_cat_Image.py

--- Dog_Cat_Classification/4) Dog_cat_Image.py
+++ Dog_Cat_Classification/4) Dog_cat_Image.py
@@ -1,34 +1,35 @@
 # Classification of Dogs and cats
 import numpy as np
+import matplotlib.pyplot as plt
 from keras.models import load_model
+from sklearn.model_selection import train_test_split
 import cv2
 
-model = load_model('Dog_cat.model')
+model = load_model('dog_cat.model')
 print('Model Loaded Sucessfully.')
 
 while True:
     filename = input('\nPlease enter the file name along with ext. >>> ')
 
-    path = 'X:/Users/...../Desktop/{}'.format(filename)
+    path = 'C:/Users/.../Desktop/{}'.format(filename)
 
     img = cv2.imread(path, cv2.IMREAD_GRAYSCALE)
     img = cv2.resize(img, (50, 50))
     img = np.array(img)
 
-##    cv2.imshow('Image', img)
-##    cv2.waitKey(0)
-##    cv2.destroyAllWindows()
-
-    img = img.reshape(1, 50, 50, 1)
-    pred = model.predict(img)
+    img_NN = img.reshape(1, 50, 50, 1)
+    pred = model.predict(img_NN)
 
     if np.argmax(pred[0]) == 0:
-        print('The given image is of a CAT!\n')
+        print('\n\tThe given image is of a CAT!')
     elif np.argmax(pred[0]) == 1:
-        print('The given image is of a DOG!\n')
+        print('\n\tThe given image is of a DOG!')
 
     print('Percentage of the image containing a CAT is', (pred[0][0] * 100))
     print('Percentage of the image containing a DOG is', (pred[0][1] * 100))
+
+    plt.imshow(img, cmap=plt.cm.gray_r, interpolation='nearest')
+    plt.show()
 
     ui = input("\nPress ENTER to continue, 'q' to exit...")
 
