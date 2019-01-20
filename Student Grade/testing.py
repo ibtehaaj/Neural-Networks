954564bb1379543ccbb2a3bcd20726afce39d7ae
Gautam J <34481342+Gautam-J@users.noreply.github.com>
2019-01-20 17:07:15 +0530

    Update testing.py

--- Student Grade/testing.py
+++ Student Grade/testing.py
@@ -1,5 +1,6 @@
 import numpy as np
 from keras.models import load_model
+from sklearn.preprocessing import MinMaxScaler
 
 print('Loading Model...')
 model = load_model('student.model')
@@ -13,9 +14,13 @@ while True:
         absence = float(input('Number of absences (0-93): '))
         g1 = float(input('Marks in Test 1 (0-20): '))
         g2 = float(input('Marks in Test 2 (0-20): '))
-
         x = np.array([[fail, absence, g1, g2]])
+
+        scaler = MinMaxScaler()
         pred = model.predict(x)
+        print(pred)
+        scaler.fit(pred)
+        pred = scaler.inverse_transform(pred)
         print(pred)
 
     else:
