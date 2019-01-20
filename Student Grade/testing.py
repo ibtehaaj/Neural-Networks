0a79a1aa93a7ec7a7e5ab56480fc6a25b6f900ff
Gautam J <34481342+Gautam-J@users.noreply.github.com>
2019-01-20 12:25:48 +0530

    Add files via upload

---
+++ Student Grade/testing.py
@@ -0,0 +1,22 @@
+import numpy as np
+from keras.models import load_model
+
+print('Loading Model...')
+model = load_model('student.model')
+print('Model Loaded Successfully!\n')
+
+while True:
+    ui = input('Press ENTER to enter the details according to the format, "q" to exit:')
+
+    if ui != 'q':
+        fail = float(input('Number of failures (0-3): '))
+        absence = float(input('Number of absences (0-93): '))
+        g1 = float(input('Marks in Test 1 (0-20): '))
+        g2 = float(input('Marks in Test 2 (0-20): '))
+
+        x = np.array([[fail, absence, g1, g2]])
+        pred = model.predict(x)
+        print(pred)
+
+    else:
+        break
---
+++ Student Grade/training.py
@@ -0,0 +1,56 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from keras.models import Sequential
+from keras.layers import Dense
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import MinMaxScaler
+
+por_dir = "F:\\Gautam\\Tech Stuff\\Python Projects\\Datasets\\student\\student-por.csv"
+math_dir = "F:\\Gautam\\Tech Stuff\\Python Projects\\Datasets\\student\\student-mat.csv"
+
+df1 = pd.read_csv(por_dir, sep=';')
+df2 = pd.read_csv(math_dir, sep=';')
+df = pd.concat([df1, df2], ignore_index=True)
+
+features = ['failures', 'absences', 'G1', 'G2']
+x = df[features]
+y = df['G3']
+# failures = n if 1<=n<3 else 4
+# absences = 0-93
+# G1 = 0-20
+# G2 = 0-20
+# G3 = 0-20
+
+x = np.array(x)
+y = np.array(y).reshape(y.shape[0], 1)
+
+scaler = MinMaxScaler()
+x = scaler.fit_transform(x)
+y = scaler.fit_transform(y)
+
+train_x, test_x, train_y, test_y = train_test_split(x, y, test_size=0.1,
+                                                    random_state=42)
+
+model = Sequential()
+
+model.add(Dense(12, activation='relu', input_dim=4))
+model.add(Dense(8, activation='relu'))
+model.add(Dense(1, activation='linear'))
+model.compile(loss='mse', optimizer='adam', metrics=['mse', 'mae'])
+
+history = model.fit(train_x, train_y, epochs=150, verbose=2,
+                    validation_split=0.1)
+
+plt.plot(history.history['loss'], label='Loss')
+plt.plot(history.history['val_loss'], label='Validation Loss')
+plt.legend()
+plt.show()
+
+score = model.evaluate(test_x, test_y)
+print(score)
+# model.save('student.model')
+
+for i in range(len(test_x)):
+    pred = model.predict(test_x[i].reshape((1, 4)))
+    print(f'Input {test_x[i]}, Prediction {pred} Actual {test_y[i]}')
