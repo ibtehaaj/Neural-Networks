7919c1b366b10b5b39e5463ee8b01f922995583c
Gautam J <34481342+Gautam-J@users.noreply.github.com>
2019-06-09 13:02:54 +0530

    Update sarcasm.py

--- Sarcasm/sarcasm.py
+++ Sarcasm/sarcasm.py
@@ -1,8 +1,5 @@
 import json
 import numpy as np
-import matplotlib.pyplot as plt
-import seaborn as sns
-from sklearn.metrics import confusion_matrix
 from sklearn.model_selection import train_test_split
 from keras.preprocessing.text import Tokenizer
 from keras.preprocessing.sequence import pad_sequences
